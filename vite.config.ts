1346fce24d906a3a84de19e9ec1386c08e26eb8c
stellalatim4-byte <stellalatim4@gmail.com>
2026-02-08 07:31:20 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
-    return {base:"/ai-studio-app-1/",plugins:[react()],};
+    return {base:"/ai-studio-app-1-sync/",plugins:[react()],};
       server: {
         port: 3000,
         host: '0.0.0.0',
